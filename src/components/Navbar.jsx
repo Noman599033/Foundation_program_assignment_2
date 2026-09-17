84fc390e72e1a3ca0599f0c36bd3b45f72ecfb31
Abdullah Noman <abdnoman55@gmail.com>
2026-09-17 16:50:30 +0600

    home button removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,15 @@ const Navbar = () => {
 
                 <div>
                     <h1 className="text-2xl font-bold">
-                        <i class="fa-solid fa-clapperboard"></i> MovieExplorer
+                        <i class="fa-solid fa-clapperboard"></i>
+                        <a href="/" className="ml-2 hover:text-yellow-400">
+                         MovieExplorer 
+                        </a> 
                     </h1>
                 </div>
 
                 <div className="flex items-center gap-6">
-                    <a href="/" className="hover:text-yellow-400">
-                        Home
-                    </a>
+                    
 
                     <a href="/movies" className="hover:text-yellow-400">
                         Movies
